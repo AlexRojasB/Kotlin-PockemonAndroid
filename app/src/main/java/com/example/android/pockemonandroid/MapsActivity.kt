dfcb82a9a55ce535d737dac35c226fc67bfa873d
Alexander Rojas <alexrrojas.b@gmail.com>
2018-05-25 14:16:02 -0600

    gathering loc

--- app/src/main/java/com/example/android/pockemonandroid/MapsActivity.kt
+++ app/src/main/java/com/example/android/pockemonandroid/MapsActivity.kt
@@ -1,14 +1,25 @@
 package com.example.android.pockemonandroid
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlin.concurrent.thread
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,8 +32,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        checkPermission()
     }
 
+    var accessLocation = 123
+    fun checkPermission(){
+        if(Build.VERSION.SDK_INT >= 23){
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), accessLocation)
+                return
+            }
+        }
+        GetUserLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun GetUserLocation(){
+       var userLocation = UserLocationListener()
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3,3f, userLocation)
+        var locThread = locationThread()
+        locThread.start()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            accessLocation ->{
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    GetUserLocation()
+                }else{
+                    Toast.makeText(this, "We cannot access to the user Location", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -34,10 +79,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
- 
+
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions()
+                .position(sydney)
+                .title("Me")
+                .snippet(" here is my location")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14f))
+    }
+    var location:Location?= null
+
+    inner class UserLocationListener:LocationListener{
+
+        constructor(){
+            location = Location("Start")
+            location!!.longitude = 0.0
+            location!!.latitude = 0.0
+
+        }
+
+        override fun onLocationChanged(locationLocal: Location?) {
+            location = locationLocal
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+        }
+
+    }
+
+    inner class  locationThread:Thread {
+        constructor():super(){
+
+        }
+
+        override fun run() {
+           while (true){
+               try {
+                   runOnUiThread {
+                       mMap.clear()
+                       val userLoc = LatLng(location!!.latitude, location!!.longitude)
+                       mMap.addMarker(MarkerOptions()
+                               .position(userLoc)
+                               .title("Me")
+                               .snippet(" here is my location")
+                               .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
+                       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 14f))
+                   }
+                   Thread.sleep(1000)
+               }catch (ex:Exception){
+
+               }
+           }
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/pockemonandroid/Pockemons.kt
@@ -0,0 +1,14 @@
+package com.example.android.pockemonandroid
+
+class Pockemon {
+    var Name:String ?=null
+    var Descr:String ?=null
+    var Image:Integer ?=null
+    var Power:Double ?=null
+    var Lat:Double ?=null
+    var Long:Double ?=null
+
+    constructor(name:String, image:Integer, des:String, power:Double, lat:Double, log:Double){
+
+    }
+}
